762c75d627668a501596728277f2a21d8aaf3574
Cloud-FlyTech <nimingniming750@gmail.com>
2025-05-03 15:43:00 +0900

    index.tsx を作成

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '30vh' }}>
+      <h1>Welcome to Supec Nutrarize</h1>
+      <p>The future of live streaming starts here.</p>
+    </div>
+  );
+}
